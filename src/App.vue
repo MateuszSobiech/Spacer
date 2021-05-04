fb4b2dfc070b09694c3686237fb810e29365341d
MateuszSobiech <42298749+MateuszSobiech@users.noreply.github.com>
2021-05-04 22:30:14 +0200

    Add loader

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
           :key="item.data[0].nasa_id"
         />
       </div>
+      <div class="lds-dual-ring" v-if="step === 1 && loading" />
     </div>
   </div>
 </template>
@@ -109,6 +110,31 @@ body {
 
   @media (min-width: 768px) {
     grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+
+.lds-dual-ring {
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.lds-dual-ring:after {
+  content: ' ';
+  display: block;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border-radius: 50%;
+  border: 6px solid #000;
+  border-color: #000 transparent #000 transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
